1e28273aa93ea170920c0a621034c545d1996abe
avbravo <avbravo@avbravo>
2022-06-21 12:02:04 -0500

    version beta

---
+++ src/main/java/com/avbravo/trace/interfaces/IUser.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.avbravo.trace.interfaces;
+
+
+import com.avbravo.jmoordb.core.annotation.AutoImplement;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author avbravo
+ */
+@AutoImplement(as = "User", builder = true)
+public interface IUser {
+
+//    @Mandatory
+    String getFirstName();
+
+//    @Mandatory
+    String getLastName();
+
+    LocalDate getDateOfBirth();
+
+    String getPlaceOfBirth();
+
+    String getPhone();
+
+    String getAddress();
+}
